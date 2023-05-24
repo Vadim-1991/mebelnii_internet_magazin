cf3be26dfd9c7b744163207a2dbfa5cdc87bcd8d
Vadim <vadim@mail.ru>
2023-05-24 20:58:14 +0300

    послежние изменения

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("burger").addEventListener("click", function() {
-        document.querySelector("header").classList.toggle("open")
+document.addEventListener('DOMContentLoaded', function () {
+    let burger = document.getElementById('burger');
+    burger.addEventListener("click", function () {
+        burger.classList.toggle('open');
     })
-})
+});
